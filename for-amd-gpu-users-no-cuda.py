16a85c0a3768b53c2615f98159bd094419e01092
bubbles the dev <152947339+KernFerm@users.noreply.github.com>
2024-07-17 14:12:41 -0400

    Update for-amd-gpu-users-no-cuda.py

--- for-amd-gpu-users-no-cuda.py
+++ for-amd-gpu-users-no-cuda.py
@@ -1,3 +1,7 @@
+# add this if you have no cuda aka nvidia for AMD gpu users to use cpu - put below in top part of main.py 
+
+# you will see how to place it, make sure to remove the old part with the new and remember to remove any duplicate import statements, if necessary.
+
 import torch
 
 def load_model(pt_file_path):
